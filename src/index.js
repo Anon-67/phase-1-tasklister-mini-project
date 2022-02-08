423e15aa844effd6089cf15d687d9ed7ebe1f642
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-08 08:24:27 -0600

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildTasks(e.target.new_task_description.value);
+    form.reset();    
+  })
 });
+
+
+
+function buildTasks(tasks) {
+  let list = document.createElement('li');
+  list.textContent = `${tasks}  `;
+  document.querySelector('#tasks').appendChild(list);
+
+  let btn = document.createElement('button');
+  btn.textContent = 'X';
+  list.appendChild(btn);
+  btn.addEventListener('click', (e) => {
+    e.target.parentNode.remove();
+
+  })
+
+}
